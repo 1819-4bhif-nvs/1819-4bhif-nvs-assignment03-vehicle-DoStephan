9bedbd70795b329c3c17800cdf90dcd2f1e58674
DoStephan <stephan.do08@gmail.com>
2018-10-21 20:55:54 +0200

    5. Rest mit Entity

---
+++ Vehicle/src/main/java/model/Vehicle.java
@@ -0,0 +1,52 @@
+package model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Vehicle {
+
+    private Long id;
+    private String brand;
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Vehicle() {
+
+    }
+
+    public Vehicle(String brand, String type) {
+        this.brand = brand;
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+//    @Override
+//    public String toString() {
+//        return "Vehicle{" +
+//                "brand='" + brand + '\'' +
+//                ", type='" + type + '\'' +
+//                '}';
+//    }
+}
